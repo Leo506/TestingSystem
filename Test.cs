c363a23db947cc2cd5e1ae4c6988e4defa492791
Artem Levin <leoartemlev@yandex.ru>
2022-05-17 17:27:41 +0500

    Написал класс, представляющий тест

---
+++ Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Testing
+{
+    public class Test
+    {
+        public int QuestionCount { get => questions.Count; }
+        public float Statistic { get => (float) questions.Where(q => q.GetState() == QuestionStates.CORRECT_ANSWER).Count() / questions.Count; }
+
+        private List<Question> questions;
+
+
+        public Test()
+        {
+            questions = new List<Question>();
+        }
+
+
+        public void AddQuestion(Question question)
+        {
+            questions.Add(question);
+        }
+
+        public Question GetQuestion(int index) => questions[index];
+
+        public void SelectAnswer(int questionIndex, int answerIndex)
+        {
+            Question question = questions[questionIndex];
+            if (question.GetCorrectAnswer() == answerIndex)
+                question.SetState(QuestionStates.CORRECT_ANSWER);
+            else
+                question.SetState(QuestionStates.UNCORRECT_ANSWER);
+        }
+    }
+}
