891893eeacc83f88055c3802f6e7daa26c62f633
Artem Levin <leoartemlev@yandex.ru>
2022-05-26 19:27:44 +0500

    Добавил класс настроек

---
+++ Settings/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingSystem.Settings
+{
+    public class Settings
+    {
+
+
+        public static string? GetSettings(string settingsName)
+        {
+            return ConfigurationManager.AppSettings[settingsName];
+        }
+    }
+}
